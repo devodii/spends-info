70d8d758a7975f9ec93cf2e690fb1fdb183e06a8
devodii <emmanuelodii80@gmail.com>
2024-12-31 16:43:24 +0100

    feat(ui): introduce button components

---
+++ src/components/loading-button.tsx
@@ -0,0 +1,25 @@
+import { Loader } from "lucide-react";
+import { Button, ButtonProps } from "./ui/button";
+import { cn } from "@/lib/utils";
+import { useFormStatus } from "react-dom";
+
+interface Props extends ButtonProps {
+  text: string;
+}
+
+export const LoadingButton = ({
+  children,
+  className,
+  ...forwardedProps
+}: Props) => {
+  const { pending } = useFormStatus();
+  return (
+    <Button
+      className={cn("flex gap-2 items-center justify-center", className)}
+      {...forwardedProps}
+    >
+      {pending && <Loader className="mr-2 size-4 animate-spin" />}
+      <span className="text-md font-medium">{children}</span>
+    </Button>
+  );
+};
